2493d767e73a1094e2e68334653675441acf4e67
Sam Picco Tarullo <68030022+starullo@users.noreply.github.com>
2021-03-23 21:17:23 -0500

    set up register

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import {Route, Link} from 'react-router-dom'
+
+// import Login from './Login'
+import Register from './Register'
 
 function App() {
+
+// useEffect(()=>{
+
+// }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link>
+      {/* <Route component={Login} exact path="/login"/> */}
+      <Route component={Register} exact path="/register"/>
     </div>
   );
 }
---
+++ src/Register.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react'
+import axios from "axios"
+
+const initialFormValues = {name: "", password: ""}
+
+const Register = () => {
+    const [formValues, setFormValues] = useState(initialFormValues)
+
+    const handleChange = evt => {
+        evt.preventDefault()
+        setFormValues({...formValues, [evt.target.name]: evt.target.value})
+    }
+
+    const handleSubmit = evt => {
+        evt.preventDefault()
+        console.log(formValues)
+        axios.post('http://localhost:4343/auth/register', formValues)
+        .then(res=>{
+            console.log(res)
+        })
+        .catch(err=>{
+            console.log(err.message)
+        })
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name
+                    <input
+                    type="text"
+                    value={formValues.name}
+                    onChange={handleChange}
+                    name="name"/>
+                </label>
+                <label htmlFor="password">Password
+                    <input
+                    type="password"
+                    value={formValues.password}
+                    onChange={handleChange}
+                    name="password"/>
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Register
